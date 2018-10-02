2c2b1e344cce25ab450b6dc56dc5a52904f901b5
omar-nav <omar.navarro@g.austincc.edu>
2018-10-02 16:19:54 -0500

    first commit

--- src/components/forms/FormContainer.js
+++ src/components/forms/FormContainer.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import FormDisplay from "./FormDisplay";
 class FormContainer extends Component {
   state = {
-    form: {}
+    form: {},
+    products: []
   };
 
   onChange = e => {
@@ -10,9 +11,17 @@ class FormContainer extends Component {
     const value = e.target.value;
     const { form } = this.state;
     form[field] = value;
+    this.setState({ form });
   };
-
+  // onSubmit agregar los valores al state
+  onSubmit = e => {
+    e.preventDefault();
+    const { form, products } = this.state;
+    products.push(form);
+    this.setState({ products });
+  };
   render() {
+    console.log(this.state.form);
     return (
       <div>
         <FormDisplay onChange={this.onChange} />
